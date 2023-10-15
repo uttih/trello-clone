3a0cedec0e0528be65de6813a397f110c3fc58ea
wkjy <u.hikaru0830@gmail.com>
2023-10-15 23:29:02 +0900

    2023/10/15
    TaskCardTitle 6-29

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TaskCardTitle = () => {
-  return <div>TaskCardTitle</div>;
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Today");
+
+  const handleClick = () => {
+    setIsClick(true);
+  };
+
+  const handleChange = (e) => {
+    setInputCardTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} />
+        </form>
+      ) : (
+        <h3>{inputCardTitle}</h3>
+      )}
+    </div>
+  );
 };
