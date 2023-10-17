350d3f69df2922d42ebf00dc58ab9531ed417f55
wkjy <u.hikaru0830@gmail.com>
2023-10-17 15:53:36 +0900

    2023/10/17
    7-42

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
 
 export const TaskAddInput = ({
   inputText,
@@ -7,8 +8,8 @@ export const TaskAddInput = ({
   taskList,
 }) => {
   const handleSubmit = (e) => {
+    const taskId = uuid();
     e.preventDefault();
-    console.log(e);
     if (inputText === "") {
       return;
     }
@@ -16,8 +17,8 @@ export const TaskAddInput = ({
     setTaskList([
       ...taskList,
       {
-        id: taskList.length,
-        draggableId: `task-${taskList.length}`,
+        id: taskId,
+        draggableId: `task-${taskId}`,
         text: inputText,
       },
     ]);
